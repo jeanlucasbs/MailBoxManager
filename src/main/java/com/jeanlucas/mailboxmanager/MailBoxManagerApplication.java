4cd63a396bfa5a778a301a78db4a54883a05e496
jeanlucasbs <jean.silva@discente.ufg.br>
2024-09-14 09:04:01 -0300

    First commit
    Implementando configuração de API de gerenciador de email com Spring

---
+++ src/main/java/com/jeanlucas/mailboxmanager/MailBoxManagerApplication.java
@@ -0,0 +1,13 @@
+package com.jeanlucas.mailboxmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailBoxManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailBoxManagerApplication.class, args);
+    }
+
+}
